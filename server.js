74c50c245c394c1e4912370f5eeb8e3a5e74174f
JG HAN <gojkhan13@gmail.com>
2022-02-17 01:44:50 +0900

    0217 저장 확인

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ MongoClient.connect('mongodb+srv://admin:3shan212406@cluster0.nuqju.mongodb.net/
 
     db = client.db('todoapp');
 
+    
+
     app.listen(8070, function(){
         console.log('listening on 8070')
     });
@@ -30,6 +32,11 @@ app.get('/write', function(req, res){
 
 app.post('/add', function(req, res){
     res.send('전송완료');
+
+    db.collection('post2').insertOne({제목 :req.body.title, 날짜 : req.body.date}, function(err, rst){
+        console.log('저장완료')
+    })
+    
     console.log(req.body.title); 
     console.log(req.body.date); 
 
