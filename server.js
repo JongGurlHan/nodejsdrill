7c54a03081038c497ac698cb4d6334ad33ee3df7
JG HAN <gojkhan13@gmail.com>
2022-02-22 01:46:59 +0900

    0222 로그인기능

--- server.js
+++ server.js
@@ -107,3 +107,58 @@ app.put('/edit', function(req, res){
     })
     
 })
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized:false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/login', function(req, res){
+    res.render('login.ejs')
+});
+
+//1. 누가 로그인하면 locall 방식으로 아이디, 비번 인증
+app.post('/login', passport.authenticate('local', {
+    failureRedirect : '/fail'
+}), function(req, res){
+    res.redirect('/')
+});
+
+//아이디 비번 인증
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (err, rst) {
+        if (err) return done(err)
+
+        if (!rst) return done(null, false, { message: '존재하지않는 아이디요' })
+        if (입력한비번 == rst.pw) {
+            return done(null, rst)
+        } else {
+            return done(null, false, { message: '비번틀렸어요' })
+        }
+    })
+}));
+
+//로그인 성공 -> 세션정보를 만듦 -> 마이페이지 방문시 세션검사
+
+//3. 인증성공하면 세션,쿠키 만들어줌
+//세션을 저장시키는 코드(로그인 성공시 발동)
+//위의 return done(null, rst)에서의 rst가 user로 들어간다 
+passport.serializeUser(function(user, done){
+    done(null, user.id) //id를 이용해서 세션을 저장시키는 코드(로그인 성공시 발동) 이후 세션데이터를 만들고 브라우저로 세션의 id정보를 쿠키로 보냄
+
+});
+
+//이 세션 데이터를 가진 사람을 db에서 찾아주세요(마이페이지 접속시 발동)
+passport.deserializeUser(function(아이디, done){
+    done(null, {})
+});
