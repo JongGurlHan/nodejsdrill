1389f4b27740cc20c3d9d2855ef74a57d1597fba
JG HAN <gojkhan13@gmail.com>
2022-02-23 23:31:35 +0900

    0223 검색기능 완료

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ MongoClient.connect(process.env.DB_URL, function(err, client){
 
 app.get('/', function(req, res){
     // res.sendFile(__dirname +'/index.html')
-    res.render('index.ejs', {user : req.user});
+    res.render('index.ejs');
 
 })
 
@@ -63,6 +63,31 @@ app.get('/list', function(req, res){
         console.log(rst)
         res.render('list.ejs', {posts : rst}, );
     });
+})
+
+//특정 게시물 조회
+app.get('/search', (req, res) => {
+
+    var 검색조건 = [
+        {
+          $search: {
+            index: 'titleSearch',
+            text: {
+              query: req.query.value,
+              path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+            }
+          }
+        }
+        // {$sort : {_id : 1}} //결과정렬: _id순
+      ] 
+      
+
+
+
+    db.collection('post').aggregate(검색조건).toArray((err, rst) =>{
+        console.log(rst);
+        res.render('search.ejs', {posts : rst})
+    })
 })
 
 app.delete('/delete', function(req, res){
